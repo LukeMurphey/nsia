f7d4d76f36947039a0ab18d55268e94b9cb59a86
Luke <Luke@LukeMurphey.net>
2009-04-11 06:20:56 +0000

    Added win32 service

---
+++ src/win32service/ManagerService/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.ServiceProcess;
+
+namespace ManagerService
+{
+
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] Args)
+        {
+            // 1 -- Determine if the user is attempting to install or uninstall the service
+            /*Boolean install = false;
+            Boolean uninstall = false;
+
+            if (Args.Length > 1 && Args[1].Equals("install", StringComparison.CurrentCultureIgnoreCase))
+                install = true;
+
+            if (Args.Length > 1 && Args[1].Equals("uninstall", StringComparison.CurrentCultureIgnoreCase))
+                uninstall = true;
+            
+            // 1.1 -- Install the service and exit
+            if (install)
+            {
+                ManagerServiceInstaller serviceInstaller = new ManagerServiceInstaller();
+                serviceInstaller.
+            }*/
+
+            // 2 -- Run the service
+            ServiceBase.Run(new ManagerServiceBase());
+        }
+    }
+}
