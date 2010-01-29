a678bb940bbfde17744885c7ab4ccc4f2b371592
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-01-29 06:03:31 +0000

    Added ability to set default message in pattern validator

--- trunk/src/net/lukemurphey/nsia/web/forms/PatternValidator.java
+++ trunk/src/net/lukemurphey/nsia/web/forms/PatternValidator.java
@@ -11,6 +11,7 @@ import java.util.regex.Pattern;
 public class PatternValidator implements FieldValidator {
 
 	private Pattern pattern = null;
+	private String defaultMessage = null;
 	
 	public PatternValidator( String pattern ){
 		
@@ -34,15 +35,39 @@ public class PatternValidator implements FieldValidator {
 		this.pattern = pattern;
 	}
 	
+	public PatternValidator( String pattern, String message ){
+		
+		// 0 -- Precondition check
+		if( pattern == null ){
+			throw new IllegalArgumentException("The pattern cannot be null");
+		}
+		
+		// 1 -- Initialize the class
+		this.pattern = Pattern.compile(pattern);
+		this.defaultMessage = message;
+	}
+	
+	public PatternValidator( Pattern pattern, String message ){
+		
+		// 0 -- Precondition check
+		if( pattern == null ){
+			throw new IllegalArgumentException("The pattern cannot be null");
+		}
+		
+		// 1 -- Initialize the class
+		this.pattern = pattern;
+		this.defaultMessage = message;
+	}
+	
 	@Override
 	public FieldValidatorResponse validate(String value) {
 		Matcher matcher = pattern.matcher(value);
 		
 		if( matcher.matches() == false ){
-			return new FieldValidatorResponse(false);
+			return new FieldValidatorResponse(false, defaultMessage);
 		}
 		else{
-			return new FieldValidatorResponse(true);
+			return new FieldValidatorResponse(true, defaultMessage);
 		}
 	}
 
