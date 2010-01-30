4cbf87ade456258c51ecdbc6b7a2364863cf2ab7
Luke <Luke@LukeMurphey.net>
2010-01-30 06:14:33 +0000

    Updated the email address validator such that the message can be changed

--- src/net/lukemurphey/nsia/web/forms/EmailAddressValidator.java
+++ src/net/lukemurphey/nsia/web/forms/EmailAddressValidator.java
@@ -7,15 +7,28 @@ import net.lukemurphey.nsia.InvalidLocalPartException;
 
 public class EmailAddressValidator implements FieldValidator {
 
+	private String defaultMessage = null;
+	
+	public EmailAddressValidator(){ }
+	public EmailAddressValidator( String defaultMessage ){
+		this.defaultMessage = defaultMessage;
+	}
+	
 	@Override
 	public FieldValidatorResponse validate(String value) {
+		
+		String msg = defaultMessage;
+		
+		if( msg == null ){
+			msg = "Email address is not valid";
+		}
 		
 		try {
 			EmailAddress.getByAddress(value);
 		} catch (UnknownHostException e) {
-			return new FieldValidatorResponse(false, "The domain is invalid");
+			return new FieldValidatorResponse(false, msg + " (domain is invalid)");
 		} catch (InvalidLocalPartException e) {
-			return new FieldValidatorResponse(false, "The local part is invalid (part before the @ symbol)");
+			return new FieldValidatorResponse(false, msg + " (local part is invalid [part before the @ symbol])");
 		}
 		
 		return new FieldValidatorResponse(true);
