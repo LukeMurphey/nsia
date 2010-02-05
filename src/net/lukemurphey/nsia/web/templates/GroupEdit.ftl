8b1fdecdf34f3f2b8d642dd06bda76019a7c80c3
Luke <Luke@LukeMurphey.net>
2010-02-05 23:08:16 +0000

    Added methods for managing user groups via the web interface

---
+++ src/net/lukemurphey/nsia/web/templates/GroupEdit.ftl
@@ -0,0 +1,30 @@
+<#include "GetURLFunction.ftl">
+<#include "GetDialog.ftl">
+<#include "Forms.ftl">
+
+<#assign content>
+    <div><span class="Text_1">Group Management</span>
+    <br><span class="LightText"><#if group??>Edit<#else>Create New</#if> Group</span>
+    <#if (form_errors??)>
+    <@getFormErrors form_errors=form_errors />
+    </#if>
+    <form action="<#if !group??><@url name="group_editor" args=["New"]/><#else><@url name="group_editor" args=["Edit", group.groupId]/></#if>" method="post">
+        <#if group??><input type="hidden" name="GroupID" value="${group.groupId}"></#if>
+        <table class="DataTable">
+            <tr class="<#if (form_errors?? && form_errors.fieldHasError("Name"))>ValidationFailed<#else>Background1</#if>">
+                <td class="Text_3">Site Group Name</td>
+                <td><input class="textInput" style="width: 350px;" type="text" name="Name" value="<#if request.getParameter("Name")??>${request.getParameter("Name")}<#elseif group??>${group.groupName}</#if>"></td>
+            </tr>
+            <tr style="vertical-align:top" class="<#if (form_errors?? && form_errors.fieldHasError("Description") )>ValidationFailed<#else>Background1</#if>">
+                <td class="Text_3">Description</td>
+                <td><textarea style="width: 350px;" rows="8" name="Description"><#if request.getParameter("Description")??>${request.getParameter("Description")}<#elseif group??>${group.description}</#if></textarea></td>
+            </tr>
+            <tr class="Background3">
+                <td class="alignRight" colspan="2">
+                    <#if !group??><input class="button" type="Submit" name="Submit" value="Create Group"><#else><input class="button" type="Submit" name="Submit" value="Apply Changes"></#if>
+                </td>
+            </tr>
+        </table>
+    </form>
+</#assign>
+<#include "BaseWithNav.ftl">
---
+++ src/net/lukemurphey/nsia/web/views/GroupEnableView.java
@@ -0,0 +1,85 @@
+package net.lukemurphey.nsia.web.views;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.lukemurphey.nsia.Application;
+import net.lukemurphey.nsia.GroupManagement;
+import net.lukemurphey.nsia.NoDatabaseConnectionException;
+import net.lukemurphey.nsia.GroupManagement.GroupDescriptor;
+import net.lukemurphey.nsia.web.RequestContext;
+import net.lukemurphey.nsia.web.URLInvalidException;
+import net.lukemurphey.nsia.web.View;
+import net.lukemurphey.nsia.web.ViewFailedException;
+import net.lukemurphey.nsia.web.ViewNotFoundException;
+import net.lukemurphey.nsia.web.SessionMessages.MessageSeverity;
+import net.lukemurphey.nsia.web.views.Dialog.DialogType;
+
+public class GroupEnableView extends View {
+
+	public static final String VIEW_NAME = "group_enable";
+	
+	public GroupEnableView() {
+		super("Group/Enable", VIEW_NAME, Pattern.compile("[0-9]+"));
+	}
+
+	public static String getURL( GroupDescriptor group ) throws URLInvalidException{
+		GroupEnableView view = new GroupEnableView();
+		
+		return view.createURL(group.getGroupId());
+	}
+	
+	@Override
+	protected boolean process(HttpServletRequest request,
+			HttpServletResponse response, RequestContext context,
+			String[] args, Map<String, Object> data)
+			throws ViewFailedException, URLInvalidException, IOException,
+			ViewNotFoundException {
+
+		// 0 -- Check permissions
+		//TODO Check rights
+		
+		// 1 -- Get the group if one exists
+		if( args.length >= 1 ){
+			
+			// 1.1 -- Get the group ID
+			int groupID;
+			
+			try{
+				groupID = Integer.valueOf( args[0] );
+			}
+			catch( NumberFormatException e ){
+				Dialog.getDialog(response, context, data, "The Group ID provided is not valid", "Group ID Invalid", DialogType.WARNING);
+				return true;
+			}
+			
+			// 1.2 -- Enable the group
+			GroupManagement groupMgmt = new GroupManagement(Application.getApplication());
+			
+			try{
+				if( groupMgmt.enableGroup(groupID) ){
+					context.addMessage("Group successfully enabled", MessageSeverity.SUCCESS);
+					response.sendRedirect(GroupListView.getURL()); //TODO Replace with view of site group 
+					return true;
+				}
+				else{
+					Dialog.getDialog(response, context, data, "No group was found with the given ID", "Group Not Found", DialogType.WARNING);
+					return true;
+				}
+			} catch (SQLException e) {
+				throw new ViewFailedException(e);
+			} catch (NoDatabaseConnectionException e) {
+				throw new ViewFailedException(e);
+			}
+		}
+		
+		return true;
+		
+	}
+
+}
