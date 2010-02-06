513898b42ccdf3577d7d6d734f543e51136f642b
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-02-06 21:33:23 +0000

    * Updated rule edit view to allow selecting which rule to create * Added view selector for service scan rule

--- trunk/src/net/lukemurphey/nsia/web/views/RuleEditView.java
+++ trunk/src/net/lukemurphey/nsia/web/views/RuleEditView.java
@@ -18,6 +18,7 @@ import net.lukemurphey.nsia.SiteGroupManagement.SiteGroupDescriptor;
 import net.lukemurphey.nsia.scan.HttpSeekingScanRule;
 import net.lukemurphey.nsia.scan.ScanRule;
 import net.lukemurphey.nsia.scan.ScanRuleLoader;
+import net.lukemurphey.nsia.scan.ServiceScanRule;
 import net.lukemurphey.nsia.scan.ScanRule.ScanRuleLoadFailureException;
 import net.lukemurphey.nsia.web.Link;
 import net.lukemurphey.nsia.web.RequestContext;
@@ -27,6 +28,7 @@ import net.lukemurphey.nsia.web.URLInvalidException;
 import net.lukemurphey.nsia.web.View;
 import net.lukemurphey.nsia.web.ViewFailedException;
 import net.lukemurphey.nsia.web.ViewNotFoundException;
+import net.lukemurphey.nsia.web.templates.TemplateLoader;
 import net.lukemurphey.nsia.web.views.Dialog.DialogType;
 
 public class RuleEditView extends View {
@@ -37,6 +39,18 @@ public class RuleEditView extends View {
 		super("Rule", VIEW_NAME, Pattern.compile("New|Edit", Pattern.CASE_INSENSITIVE), Pattern.compile("[0-9]*"));
 	}
 
+	public static String getURL( int ruleID ) throws URLInvalidException{
+		RuleEditView view = new RuleEditView();
+		
+		return view.createURL(ruleID);
+	}
+	
+	public static String getURL( long ruleID ) throws URLInvalidException{
+		RuleEditView view = new RuleEditView();
+		
+		return view.createURL(ruleID);
+	}
+	
 	@Override
 	protected boolean process(HttpServletRequest request,
 			HttpServletResponse response, RequestContext context,
@@ -71,10 +85,10 @@ public class RuleEditView extends View {
 				try{
 					rule = ScanRuleLoader.getScanRule(ruleID);
 				} catch(ScanRuleLoadFailureException e){
-					Dialog.getDialog(response, context, data, "No group was found with the given ID", "Group Not Found", DialogType.WARNING);
+					Dialog.getDialog(response, context, data, "No rule was found with the given ID", "Rule Not Found", DialogType.WARNING);
 					return true;
 				} catch(NotFoundException e){
-					Dialog.getDialog(response, context, data, "No group was found with the given ID", "Group Not Found", DialogType.WARNING);
+					Dialog.getDialog(response, context, data, "No rule was found with the given ID", "Rule Not Found", DialogType.WARNING);
 					return true;
 				}
 
@@ -84,40 +98,42 @@ public class RuleEditView extends View {
 				}
 
 				data.put("rule", rule);
+			}
 
-				// 2 -- Get the site group
+			// 2 -- Get the site group
 
-				//	 2.1 -- Determine the site group ID
-				if( rule != null ){
-					try{
-						siteGroupID = ScanRule.getAssociatedSiteGroup(ruleID);
-					}
-					catch(NotFoundException e1){
-						Dialog.getDialog(response, context, data, "No SiteGroup exists with the given ID", "SiteGroup Not Found", DialogType.WARNING);
-						return true;
-					}
-				}
-				else{
-					try{
-						siteGroupID = Integer.valueOf( request.getParameter("SiteGroupID") );
-					}
-					catch(NumberFormatException e1){
-						Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING);
-						return true;
-					}
-				}
-
-				//	 2.2 -- Get the Site group descriptor
+			//	 2.1 -- Determine the site group ID
+			if( rule != null ){
 				try{
-					SiteGroupManagement mgmr = new SiteGroupManagement(Application.getApplication());
-					siteGroup = mgmr.getGroupDescriptor( siteGroupID );
-				} catch (NotFoundException e1) {
-					Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING);
-					return true;
-				} catch (NumberFormatException e1) {
-					Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING);
+					siteGroupID = ScanRule.getAssociatedSiteGroup(rule.getRuleId());
+				}
+				catch(NotFoundException e1){
+					Dialog.getDialog(response, context, data, "No SiteGroup exists with the given ID", "SiteGroup Not Found", DialogType.WARNING);
 					return true;
 				}
+			}
+			else{
+				try{
+					siteGroupID = Integer.valueOf( request.getParameter("SiteGroupID") );
+				}
+				catch(NumberFormatException e1){
+					Shortcuts.addDashboardHeaders(request, response, data);
+					Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING, new Link("Return to the Main Dashboard", MainDashboardView.getURL()));
+					return true;
+				}
+			}
+			data.put("siteGroupID", siteGroupID);
+
+			//	 2.2 -- Get the Site group descriptor
+			try{
+				SiteGroupManagement mgmr = new SiteGroupManagement(Application.getApplication());
+				siteGroup = mgmr.getGroupDescriptor( siteGroupID );
+			} catch (NotFoundException e1) {
+				Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING);
+				return true;
+			} catch (NumberFormatException e1) {
+				Dialog.getDialog(response, context, data, "The SiteGroup ID provided is not valid", "SiteGroup ID Invalid", DialogType.WARNING);
+				return true;
 			}
 		} catch (SQLException e) {
 			throw new ViewFailedException(e);
@@ -126,6 +142,48 @@ public class RuleEditView extends View {
 		} catch (InputValidationException e) {
 			throw new ViewFailedException(e);
 		}
+		
+		
+		// Get the scan frequency value and units
+		int scanFrequencyUnits = 0;
+		int scanFrequencyValue = 0;
+		
+		if( request.getParameter("ScanFrequencyUnits") != null && request.getParameter("ScanFrequencyValue") != null ){
+			try{
+				scanFrequencyUnits = Integer.parseInt(request.getParameter("ScanFrequencyUnits"));
+				scanFrequencyValue = Integer.parseInt(request.getParameter("ScanFrequencyValue"));
+			}
+			catch(NumberFormatException e){
+				scanFrequencyUnits = 0;
+				scanFrequencyValue = 0;
+			}
+		}
+		else if( (scanFrequencyUnits == 0 || scanFrequencyValue == 0) && rule != null){
+			int frequency = rule.getScanFrequency();
+			if( ( frequency % 86400) == 0){ //Days
+				scanFrequencyUnits = 86400;
+				scanFrequencyValue = frequency / 86400;
+			}
+			else if( ( frequency % 3600) == 0){ //Hours
+				scanFrequencyUnits = 3600;
+				scanFrequencyValue = frequency / 3600;
+			}
+			else if( ( frequency % 60) == 0){ //Minutes
+				scanFrequencyUnits = 60;
+				scanFrequencyValue = frequency / 60;
+			}
+			else { //Seconds
+				scanFrequencyUnits = 1;
+				scanFrequencyValue = frequency;
+			}
+		}
+		else{
+			scanFrequencyUnits = 3600;
+			scanFrequencyValue = 1;
+		}
+		
+		data.put("scanFrequencyUnits", scanFrequencyUnits);
+		data.put("scanFrequencyValue", scanFrequencyValue);
 
 		// 3 -- Get the menu
 		Vector<Link> menu = new Vector<Link>();
@@ -140,14 +198,15 @@ public class RuleEditView extends View {
 
 		// 4 -- Get the breadcrumbs
 		Vector<Link> breadcrumbs = new Vector<Link>();
-		breadcrumbs.add(  new Link("Site Group", SiteGroupView.getURL(siteGroupID)) );
+		breadcrumbs.add(  new Link("Main Dashboard", MainDashboardView.getURL()) );
+		breadcrumbs.add(  new Link("Site Group: " + siteGroup.getGroupName(), SiteGroupView.getURL(siteGroupID)) );
 
 		if( rule != null ){
-			breadcrumbs.add( new Link("Edit Rule","ADDURL") );
-			data.put("title", "Rule: " + rule);
+			breadcrumbs.add( new Link("Edit Rule", createURL("Edit", rule.getRuleId())) );
+			data.put("title", "Rule: " + rule.getRuleType());
 		}
 		else{
-			breadcrumbs.add(  new Link("New Rule", createURL("New")) );
+			breadcrumbs.add(  new Link("New Rule", createURL("New") + "?SiteGroupID=" + siteGroupID) );
 			data.put("title", "New Rule");
 		}
 		data.put("breadcrumbs", breadcrumbs);
@@ -163,13 +222,26 @@ public class RuleEditView extends View {
 			ruleType = rule.getRuleType();
 		}
 		else{
-			request.getParameter("RuleType");
+			ruleType = request.getParameter("RuleType");
 		}
 		
-		//	 5.2 -- Show the view associated with the rule
-		if( ruleType == HttpSeekingScanRule.RULE_TYPE ){
+		//	 5.2 -- Show the selection form if no rule type was specified
+		if( ruleType == null ){
+			TemplateLoader.renderToResponse("SelectRule.ftl", data, response);
+			return true;
+		}
+		
+		//	 5.3 -- Show the view associated with the rule
+		if( ruleType.equals( HttpSeekingScanRule.RULE_TYPE ) ){
 			WebDiscoveryRuleEditView view = new WebDiscoveryRuleEditView();
 			return view.process(request, response, context, args, data);
+		}
+		else if( ruleType.equals( ServiceScanRule.RULE_TYPE ) ){
+			ServiceScanRuleEditView view = new ServiceScanRuleEditView();
+			return view.process(request, response, context, args, data);
+		}
+		else{
+			Dialog.getDialog(response, context, data, "The rule type provided is not recognized", "Rule Type Not Recognized", DialogType.WARNING, new Link("Select Rule Type", createURL("New") + "?SiteGroupID=" + siteGroupID) );
 		}
 		
 		return true;
