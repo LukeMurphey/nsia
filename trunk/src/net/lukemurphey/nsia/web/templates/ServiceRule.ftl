bdef747229d5be936a5e391d775c7ec26a01b868
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-02-06 21:31:54 +0000

    Added web view for editing and creating service scan rules

---
+++ trunk/src/net/lukemurphey/nsia/web/templates/ServiceRule.ftl
@@ -0,0 +1,69 @@
+<#include "GetURLFunction.ftl">
+<#include "GetDialog.ftl">
+<#include "Forms.ftl">
+
+<#assign content>
+    <div><span class="Text_1">Service Scan Rule</span>
+    <br><span class="LightText">Monitors a server to identify when new ports are opened or when existing ports are closed</span>
+    <#if (form_errors??)>
+    <@getFormErrors form_errors=form_errors />
+    </#if>
+    <script src="/media/codepress/codepress.js" type="text/javascript"></script>
+    
+    <script type="text/javascript">
+        function submitEditorForm(editorform){
+            document.editorform.PortsExpectedOpen2.value = cp2.getCode();
+            document.editorform.PortsToScan2.value = cp1.getCode();
+            document.editorform.submit();
+            return true;
+        }
+    </script>
+    
+    <form name="editorform" id="editorform" onSubmit="return submitEditorForm(this.form)" action="<#if rule??><@url name="rule_editor" args=["Edit", rule.ruleId]/><#else><@url name="rule_editor" args=["New"]/></#if>" method="post">
+        <table class="DataTable">
+            <#-- 1 -- Output scan frequency -->
+            <tr class="<#if (form_errors?? && form_errors.fieldHasError("ScanFrequencyValue"))>ValidationFailed<#else>Background1</#if>">
+                <td class="Text_3">Scan Frequency</td>
+                <td><input class="textInput" type="text" name="ScanFrequencyValue" value="${scanFrequencyValue}" />&nbsp;&nbsp;
+                    <select name="ScanFrequencyUnits">
+                        <option value="86400"<#if scanFrequencyUnits == 84600> selected</#if>>Days</option>
+                        <option value="3600"<#if scanFrequencyUnits == 3600> selected</#if>>Hours</option>
+                        <option value="60"<#if scanFrequencyUnits == 60> selected</#if>>Minutes</option>
+                        <option value="1"<#if scanFrequencyUnits == 1> selected</#if>>Seconds</option>
+                    </select>
+                </td>
+            </tr>
+            <#-- 2 -- Output server address -->
+            <tr class="<#if (form_errors?? && form_errors.fieldHasError("Server"))>ValidationFailed<#else>Background1</#if>">
+                <td class="Text_3">Server Address</td>
+                <td><input class="textInput" size="40" type="text" name="Server" value="<#if request.getParameter("Server")??>${request.getParameter("Server")}<#elseif rule??>${rule.serverAddress}</#if>"></td>
+            </tr>
+            <#-- 3 -- Output ports to scan -->
+            <tr class="<#if (form_errors?? && form_errors.fieldHasError("PortsToScan"))>ValidationFailed<#else>Background1</#if>">
+                <td style="vertical-align: top;" class="Text_3">Ports to Scan</td>
+                <td><textarea id="cp1" class="codepress ports" wrap="virtual" rows="6" cols="48" name="PortsToScan"><#if request.getParameter("PortsToScan")??>${request.getParameter("PortsToScan")}<#elseif rule??><#list rule.portsExpectedOpen as port>${port}<@endline /></#list></#if></textarea>
+                    <input type="hidden" name="PortsToScan2" value="<#if request.getParameter("PortsToScan2")??>${request.getParameter("PortsToScan2")}<#elseif rule??><#list rule.portsExpectedOpen as port>${port}<@endline /></#list></#if>"></td>
+            </tr>
+            <#-- 4 -- Output ports expected open -->
+            <tr class="<#if (form_errors?? && form_errors.fieldHasError("PortsExpectedOpen"))>ValidationFailed<#else>Background1</#if>">
+                <td style="vertical-align: top;" class="Text_3">Ports Expected Open</td>
+                <td><textarea id="cp2" class="codepress ports" wrap="virtual" rows="6" cols="48" name="PortsExpectedOpen"><#if request.getParameter("PortsExpectedOpen")??>${request.getParameter("PortsExpectedOpen")}<#elseif rule??><#list rule.portsToScan as port>${port}<@endline /></#list></#if></textarea>
+                    <input type="hidden" name="PortsExpectedOpen2" value="<#if request.getParameter("PortsExpectedOpen2")??>${request.getParameter("PortsExpectedOpen2")}<#elseif rule??><#list rule.portsToScan as port>${port}<@endline /></#list></#if>"></td>
+            </tr>
+            <tr class="lastRow">
+                <td class="alignRight" colspan="99">
+                <#if !rule?? >
+                    <input class="button" type="submit" value="Create Rule" name="Action">&nbsp;&nbsp;
+                    <input type="hidden" name="SiteGroupID" value="${siteGroupID}">
+                    <input type="hidden" name="RuleType" value="${request.getParameter("RuleType")}">
+                <#else>
+                    <input class="button" type="submit" value="Apply Changes" name="Action">&nbsp;&nbsp;
+                    <input type="hidden" name="RuleID" value="${rule.ruleId}">
+                </#if>
+                    <input class="button" type="submit" value="Cancel" name="Action">
+                </td>
+            </tr>
+        </table>
+     </form>
+</#assign>
+<#include "BaseWithNav.ftl">
---
+++ trunk/src/net/lukemurphey/nsia/web/views/ServiceScanRuleEditView.java
@@ -0,0 +1,197 @@
+package net.lukemurphey.nsia.web.views;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.lukemurphey.nsia.Application;
+import net.lukemurphey.nsia.NoDatabaseConnectionException;
+import net.lukemurphey.nsia.scan.LineParseException;
+import net.lukemurphey.nsia.scan.NetworkPortRange;
+import net.lukemurphey.nsia.scan.ScanRule;
+import net.lukemurphey.nsia.scan.ServiceScanRule;
+import net.lukemurphey.nsia.web.RequestContext;
+import net.lukemurphey.nsia.web.URLInvalidException;
+import net.lukemurphey.nsia.web.View;
+import net.lukemurphey.nsia.web.ViewFailedException;
+import net.lukemurphey.nsia.web.ViewNotFoundException;
+import net.lukemurphey.nsia.web.SessionMessages.MessageSeverity;
+import net.lukemurphey.nsia.web.forms.Field;
+import net.lukemurphey.nsia.web.forms.FieldError;
+import net.lukemurphey.nsia.web.forms.FieldErrors;
+import net.lukemurphey.nsia.web.forms.Form;
+import net.lukemurphey.nsia.web.forms.HostAddressValidator;
+import net.lukemurphey.nsia.web.forms.IntegerValidator;
+import net.lukemurphey.nsia.web.templates.TemplateLoader;
+
+public class ServiceScanRuleEditView extends View {
+
+	public static final String VIEW_NAME = "rule_editor_service_scan";
+
+	public ServiceScanRuleEditView() {
+		super("Rule", VIEW_NAME, Pattern.compile("New|Edit", Pattern.CASE_INSENSITIVE), Pattern.compile("[0-9]*"));
+	}
+
+	/**
+	 * Get a form that can validate the rule.
+	 * @return
+	 */
+	private Form getRuleForm( ){
+		Form form = new Form();
+
+		form.addField( new Field("StartAddresses") );
+		form.addField( new Field("ScanFrequencyUnits", new IntegerValidator(1,86400)) );
+		form.addField( new Field("ScanFrequencyValue", new IntegerValidator(1,1000000)) );
+		form.addField( new Field("Server", new HostAddressValidator()) );
+		form.addField( new Field("PortsToScan") );
+		form.addField( new Field("PortsExpectedOpen") );
+
+		return form;
+	}
+
+	private boolean performActions(HttpServletRequest request,
+			HttpServletResponse response, RequestContext context,
+			String[] args, Map<String, Object> data, ServiceScanRule rule) throws ViewFailedException, URLInvalidException{
+
+		Form form = getRuleForm();
+
+		FieldErrors errors = form.validate(request);
+
+		if( errors.size() > 0 ){
+			data.put("form_errors", errors);
+		}
+		else{
+			String address = request.getParameter("Server");
+			int scanFreqUnits = Integer.valueOf( request.getParameter("ScanFrequencyUnits"));
+			int scanFreqValue = Integer.valueOf( request.getParameter("ScanFrequencyValue"));
+			int scanFrequency = scanFreqUnits * scanFreqValue;
+
+			// Get the ports expected open
+			String portsExpectedOpenStr = null;
+			if( request.getParameter("PortsExpectedOpen2") != null ){
+				portsExpectedOpenStr = request.getParameter("PortsExpectedOpen2");
+			}
+			else if( request.getParameter("PortsExpectedOpen") != null ){
+				portsExpectedOpenStr = request.getParameter("PortsExpectedOpen");
+			}
+			
+			NetworkPortRange[] portsExpectedOpen;
+			
+			try{
+				portsExpectedOpen = NetworkPortRange.parseRange(portsExpectedOpenStr);
+			}
+			catch(LineParseException e){
+				errors = new FieldErrors();
+				errors.put(new FieldError("PortsExpectedOpen", "", "The list of ports expected open is invalid"));
+				data.put("form_errors", errors);
+				return false;
+			}
+			
+			// Get the ports to scan
+			String portsToScanStr = null;
+			if( request.getParameter("PortsToScan") != null ){
+				portsToScanStr = request.getParameter("PortsToScan");
+			}
+			else if( request.getParameter("PortsToScan2") != null ){
+				portsToScanStr = request.getParameter("PortsToScan2");
+			}
+			
+			if( portsToScanStr == null ){
+				errors = new FieldErrors();
+				errors.put(new FieldError("PortsToScan", "", "The list of ports to scan were not provided"));
+				data.put("form_errors", errors);
+				return false;
+			}
+			
+			NetworkPortRange[] portsToScan = null;
+			
+			try{
+				portsToScan = NetworkPortRange.parseRange(portsToScanStr);
+			}
+			catch(LineParseException e){
+				errors = new FieldErrors();
+				errors.put(new FieldError("PortsToScan", "", "The list of ports to scan is invalid"));
+				data.put("form_errors", errors);
+				return false;
+			}
+			
+			if( portsToScan.length == 0 ){
+				errors = new FieldErrors();
+				errors.put(new FieldError("PortsToScan", "", "No ports to scan were provided"));
+				data.put("form_errors", errors);
+				return false;
+			}
+			
+			boolean isNewRule = false;
+
+			if( rule == null ){
+				rule = new ServiceScanRule(Application.getApplication(), address, portsExpectedOpen, portsToScan );
+				isNewRule = true;
+			}
+			else{
+				rule.setScanFrequency(scanFrequency);
+				rule.setPortsExpectedOpen(portsExpectedOpen);
+				rule.setPortsToScan(portsToScan);
+				rule.setServerAddress(address);
+			}
+
+			try {
+				if( isNewRule ){
+					int siteGroupID = Integer.valueOf( request.getParameter("SiteGroupID") );
+					rule.saveNewRuleToDatabase(siteGroupID);
+					context.addMessage("Rule successfully created", MessageSeverity.SUCCESS);
+					response.sendRedirect( SiteGroupView.getURL(siteGroupID) );
+					return true;
+				}
+				else{
+					rule.saveToDatabase();
+					context.addMessage("Rule successfully updated", MessageSeverity.SUCCESS);
+					response.sendRedirect( SiteGroupView.getURL( ScanRule.getSiteGroupForRule(rule.getRuleId())) );
+					return true;
+				}
+
+
+			} catch (IllegalStateException e) {
+				throw new ViewFailedException(e);
+			} catch (SQLException e) {
+				throw new ViewFailedException(e);
+			} catch (NoDatabaseConnectionException e) {
+				throw new ViewFailedException(e);
+			} catch (IOException e) {
+				throw new ViewFailedException(e);
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	protected boolean process(HttpServletRequest request,
+			HttpServletResponse response, RequestContext context,
+			String[] args, Map<String, Object> data)
+	throws ViewFailedException, URLInvalidException, IOException,
+	ViewNotFoundException {
+
+		boolean viewHandled = false;
+		ServiceScanRule rule = null;
+
+		if( data.get("rule") != null ){
+			rule = (ServiceScanRule)data.get("rule");
+		}
+
+		if( request.getMethod().equalsIgnoreCase("POST") ){
+			viewHandled = performActions(request, response, context, args, data, rule);
+		}
+
+		if( viewHandled == false ){
+			TemplateLoader.renderToResponse("ServiceRule.ftl", data, response);
+		}
+
+		return true;
+	}
+
+}
