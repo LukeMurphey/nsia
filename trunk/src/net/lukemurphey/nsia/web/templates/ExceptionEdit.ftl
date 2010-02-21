17330e1737c11caddd20c15c454cc557c4d1f529
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-02-21 02:49:17 +0000

    Added views for managing exceptions

---
+++ trunk/src/net/lukemurphey/nsia/web/templates/ExceptionEdit.ftl
@@ -0,0 +1,69 @@
+<#include "GetURLFunction.ftl">
+<#include "GetDialog.ftl">
+<#include "Forms.ftl">
+<#include "Shortcuts.ftl">
+<#assign content>
+    <span class="Text_1">Definition Exception</span><p>
+    <#if (form_errors??)>
+    <@getFormErrors form_errors=form_errors />
+    </#if>
+    <form action="<@url name="exception_editor" args=[ruleID] />" method="post">
+        <input type="hidden" name="SiteGroupID" value="${siteGroupID}" />
+        <#if ( definitionName?? )>
+            <input type="hidden" name="DefinitionName" value="${definitionName}" />
+        <#else>
+            <input type="hidden" name="DefinitionID" value="${definitionID}" />
+        </#if>
+        <input type="hidden" name="RuleID" value="${ruleID}" />
+        <#if ( URL?? )>
+            <input type="hidden" name="URL" value="${URL}">
+        </#if>
+        
+        <#if ( ReturnTo?? )>
+            <input type="hidden" name="ReturnTo" value="${ReturnTo}" />
+        </#if>
+        <table>
+            <tr>
+                <td style="vertical-align: top;" rowspan="2">
+                    <input type="radio" name="FilterType" value="Definition">
+                </td>
+                <td>
+                    <span class="Text_3">Filter out definition (${definition.fullName})</span>
+                </td>
+            </tr>
+            <tr>
+                <td>Filter out findings for this specific definition only<br>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;" rowspan="2">
+                    <input type="radio" name="FilterType" value="SubCategory">
+                </td>
+                <td>
+                    <span class="Text_3">Filter out entire sub-category (${definition.categoryName}.${definition.subCategoryName})</span>
+                </td>
+            </tr>
+            <tr>
+                <td>Filter out findings for this entire sub-category (all definitions within the sub-category)<br>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="vertical-align: top;" rowspan="2">
+                    <input type="radio" name="FilterType" value="Category">
+                </td>
+                <td>
+                    <span class="Text_3">Filter out category (${definition.categoryName})</span>
+                </td>
+            </tr>
+            <tr>
+                <td>Filter out findings for this specific definition<br>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <input class="button" type="submit" value="Add Exception" name="Add Exception">&nbsp;&nbsp;
+                    <input class="button" type="submit" value="Cancel" name="Cancel">
+                </td>
+            </tr>
+        </table>
+    </form>
+</#assign>
+<#include "BaseWithNav.ftl">
---
+++ trunk/src/net/lukemurphey/nsia/web/views/ExceptionDeleteView.java
@@ -0,0 +1,90 @@
+package net.lukemurphey.nsia.web.views;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.lukemurphey.nsia.Application;
+import net.lukemurphey.nsia.NoDatabaseConnectionException;
+import net.lukemurphey.nsia.NotFoundException;
+import net.lukemurphey.nsia.scan.DefinitionPolicyManagement;
+import net.lukemurphey.nsia.web.RequestContext;
+import net.lukemurphey.nsia.web.URLInvalidException;
+import net.lukemurphey.nsia.web.View;
+import net.lukemurphey.nsia.web.ViewFailedException;
+import net.lukemurphey.nsia.web.ViewNotFoundException;
+import net.lukemurphey.nsia.web.SessionMessages.MessageSeverity;
+import net.lukemurphey.nsia.web.views.Dialog.DialogType;
+
+public class ExceptionDeleteView extends View {
+
+	public static final String VIEW_NAME = "exception_delete";
+	
+	public ExceptionDeleteView() {
+		super("Exception/Delete", VIEW_NAME, Pattern.compile("[0-9]?"));
+	}
+	
+	private int deleteExceptions( RequestContext context, Vector<Integer> exceptions ) throws SQLException, NoDatabaseConnectionException, NotFoundException{
+		
+		// 1 -- Delete the policies
+		int exceptionsDeleted = 0;
+		DefinitionPolicyManagement policyManagement = new DefinitionPolicyManagement(Application.getApplication());
+		
+		for (int exceptionID : exceptions) {
+			policyManagement.deletePolicy(exceptionID);
+			exceptionsDeleted++;
+		}
+		
+		context.addMessage(exceptionsDeleted + " exceptions have been deleted", MessageSeverity.SUCCESS);
+		
+		return exceptionsDeleted;
+		
+	}
+	
+	@Override
+	protected boolean process(HttpServletRequest request,
+			HttpServletResponse response, RequestContext context,
+			String[] args, Map<String, Object> data)
+			throws ViewFailedException, URLInvalidException, IOException,
+			ViewNotFoundException {
+		
+		// 1 -- Get the exceptions to delete
+		Vector<Integer> exceptionsToDelete = new Vector<Integer>();
+		
+		String[] identifiers = request.getParameterValues("ExceptionID");
+		
+		// Convert the exception identifiers to integers
+		for (String stringID : identifiers) {
+			
+			try{
+				exceptionsToDelete.add( new Integer( stringID ) );
+			}
+			catch(NumberFormatException e){
+				//At least one the IDs was invalid
+			}
+		}
+		
+		// 2 -- Delete the exceptions
+		try{
+			deleteExceptions(context, exceptionsToDelete);
+		}
+		catch( NotFoundException e ){
+			Dialog.getDialog(response, context, data, "Exception could not be deleted because it could not be found", "Exception Not Found", DialogType.WARNING);
+		} catch (SQLException e) {
+			throw new ViewFailedException(e);
+		} catch (NoDatabaseConnectionException e) {
+			throw new ViewFailedException(e);
+		}
+		
+		// 3 -- Get the rule ID
+		int ruleID = Integer.valueOf( args[0] );
+		response.sendRedirect( ExceptionListView.getURL(ruleID) );
+		return true;
+	}
+
+}
