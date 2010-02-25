6915bdc885d9cc746c179ade6c65de11a4838919
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-02-25 21:39:30 +0000

    Moved build scripts out of doc directory; deleting these now redundant files

--- trunk/doc/scripts/deployWar.bat
+++
@@ -1,9 +0,0 @@
-
-set corePath=C:\Users\Luke\workspace\ThreatFactor NSIA
-
-REM Clean
-DEL "%corePath%\lib\webConsole.war"
-
-REM Create the Web-Application Archive (WAR)
-cd "%corePath%\src\webarchive"
-"jar" cvf "%corePath%\lib\webConsole.war" *
--- trunk/doc/scripts/deployWar.sh
+++
@@ -1,8 +0,0 @@
-
-corePath="/home/luke"
-rm "$corePath/NSIA/lib/webConsole.war"
-
-#Create the Web-Application Archive (WAR)
-cd "$corePath/NSIA/deployment"
-jar cvf "$corePath/NSIA/lib/webConsole.war" *
-
