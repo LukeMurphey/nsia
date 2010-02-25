fefd8fec3985698c880ba1aabb2fd5e3511f1cf0
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-02-25 21:38:28 +0000

    Moved existing build scripts to sbin directory

---
+++ trunk/sbin/createjar.bat
@@ -0,0 +1,8 @@
+set corePath=../../java-bin
+
+cd %corePath%
+
+REM jar cvfm %corePath%/bin/nsia.jar %corePath%/doc/scripts/Manifest.txt %corePath%/src/template.html %corePath%/bin/net
+cp ../src/template.html .
+jar cvfm ../bin/nsia.jar ../doc/scripts/Manifest.txt template.html net
+del template.html
