6f4d18264977c4508fb511f9c9d9fac9eb56fa3a
Luke <Luke@LukeMurphey.net>
2010-03-03 03:46:04 +0000

    Changed login banner middleware such that it no longer assumes any argument of "BannerCheck" with the value of "Accept" is a request to accept the banner.

--- src/net/lukemurphey/nsia/web/middleware/LoginBannerMiddleware.java
+++ src/net/lukemurphey/nsia/web/middleware/LoginBannerMiddleware.java
@@ -23,9 +23,36 @@ public class LoginBannerMiddleware extends Middleware {
 			HttpServletResponse response, RequestContext context)
 			throws MiddlewareException {
 
-		// 1 -- Set the cookie if appropriate
+		// 1 -- Determine if the banner has already been accepted
+		if( request.getCookies() != null ){
+			for (Cookie cookie : request.getCookies()) {
+				if( "BannerCheck".equalsIgnoreCase( cookie.getName() ) && "Accept".equalsIgnoreCase( cookie.getValue() ) ){
+					return false;
+				}
+			}
+		}
+		
+		// 2 -- Determine if the application has a banner to show
+		String banner;
+		try {
+			banner = Application.getApplication().getApplicationConfiguration().getLoginBanner();
+		} catch (NoDatabaseConnectionException e1) {
+			throw new MiddlewareException(e1);
+		} catch (SQLException e1) {
+			throw new MiddlewareException(e1);
+		} catch (InputValidationException e1) {
+			throw new MiddlewareException(e1);
+		}
+		
+		if( banner == null ){ //No banner to show, just continue
+			return false;
+		}
+		
+		// 3 -- Set the cookie if appropriate
 		if( "Accept".equalsIgnoreCase( request.getParameter("BannerCheck") ) ){
-			response.addCookie( new Cookie("BannerCheck", "Accept") );
+			Cookie cookie = new Cookie("BannerCheck", "Accept");
+			cookie.setPath("/");
+			response.addCookie( cookie );
 			
 			try{
 				if( request.getParameter("ReturnTo") != null ){
@@ -48,34 +75,7 @@ public class LoginBannerMiddleware extends Middleware {
 			}
 		}
 		
-		// 2 -- Determine if the banner should be shown
-		
-		//	 2.1 -- Get the application banner (if it has one)
-		String banner;
-		try {
-			banner = Application.getApplication().getApplicationConfiguration().getLoginBanner();
-		} catch (NoDatabaseConnectionException e1) {
-			throw new MiddlewareException(e1);
-		} catch (SQLException e1) {
-			throw new MiddlewareException(e1);
-		} catch (InputValidationException e1) {
-			throw new MiddlewareException(e1);
-		}
-		
-		if( banner == null ){
-			return false;
-		}
-		
-		//	 2.2 -- Determine if the banner has already been accepted
-		if( request.getCookies() != null ){
-			for (Cookie cookie : request.getCookies()) {
-				if( "BannerCheck".equalsIgnoreCase( cookie.getName() ) && "Accept".equalsIgnoreCase( cookie.getValue() ) ){
-					return false;
-				}
-			}
-		}
-		
-		// 3 -- Execute the login banner view
+		// 4 -- Execute the login banner view
 		LoginBannerView view = new LoginBannerView();
 		try {
 			return view.process(request, response, context);
