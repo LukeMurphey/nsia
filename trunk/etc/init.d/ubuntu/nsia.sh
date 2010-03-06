618580615ddf47e7d330defb0190e2d7dce4907e
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-03-06 23:24:04 +0000

    Changed init script to point to new install directory structure

--- trunk/etc/init.d/ubuntu/nsia.sh
+++ trunk/etc/init.d/ubuntu/nsia.sh
@@ -16,7 +16,7 @@ DAEMON_OPTS='-jar /opt/nsia/bin/nsia.jar -s'
 NAME=nsia
 DESC='ThreatFactor NSIA'
 INSTALL_DIR=/opt/nsia
-PIDFILE=$INSTALL_DIR/pid
+PIDFILE=$INSTALL_DIR/var/pid
 
 test -x "$DAEMON" || exit 0
 
@@ -32,33 +32,33 @@ set -e
 case "$1" in
   start)
     echo -n "Starting $DESC: "
-    start-stop-daemon -d $INSTALL_DIR -b --start --quiet --make-pidfile --pidfile $PIDFILE \
+    start-stop-daemon -d $INSTALL_DIR/bin -b --start --quiet --make-pidfile --pidfile $PIDFILE \
         --exec "$DAEMON" -- $DAEMON_OPTS || true
     echo "$NAME."
     ;;
   stop)
     echo -n "Stopping $DESC: "
-    start-stop-daemon -d $INSTALL_DIR --stop --quiet --pidfile $PIDFILE \
+    start-stop-daemon -d $INSTALL_DIR/bin --stop --quiet --pidfile $PIDFILE \
         --exec "$DAEMON" || true
     echo "$NAME."
     ;;
   restart|force-reload)
     echo -n "Restarting $DESC: "
-    start-stop-daemon -d $INSTALL_DIR --stop --quiet --pidfile \
+    start-stop-daemon -d $INSTALL_DIR/bin --stop --quiet --pidfile \
         $PIDFILE --exec "$DAEMON" || true
     sleep 1
-    start-stop-daemon -d $INSTALL_DIR -b --start --quiet --make-pidfile --pidfile \
+    start-stop-daemon -d $INSTALL_DIR/bin -b --start --quiet --make-pidfile --pidfile \
         $PIDFILE --exec "$DAEMON" -- $DAEMON_OPTS || true
     echo "$NAME."
     ;;
   reload)
       echo -n "Reloading $DESC configuration: "
-      start-stop-daemon -d $INSTALL_DIR --stop --signal HUP --quiet --pidfile $PIDFILE \
+      start-stop-daemon -d $INSTALL_DIR/bin --stop --signal HUP --quiet --pidfile $PIDFILE \
           --exec "$DAEMON" || true
       echo "$NAME."
       ;;
   status)
-      status_of_proc -p $PIDFILE "$DAEMON" scanner_controller && exit 0 || exit $?
+      status_of_proc -p $PIDFILE "$DAEMON" "$NAME" && exit 0 || exit $?
       ;;
   *)
     N=/etc/init.d/$NAME
