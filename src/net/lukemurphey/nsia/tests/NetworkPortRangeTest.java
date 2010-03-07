87f7062e6071158169f2a5bc847eef1615f8b630
Luke <Luke@LukeMurphey.net>
2010-03-07 06:02:04 +0000

    Added tests for new methods in NetworkPortRange class that determine if one range overlaps another and a method the subtracts ranges from each other

--- src/net/lukemurphey/nsia/tests/NetworkPortRangeTest.java
+++ src/net/lukemurphey/nsia/tests/NetworkPortRangeTest.java
@@ -62,6 +62,221 @@ public class NetworkPortRangeTest extends TestCase{
 		}
 	}
 	
+	public void testRangeSplitCutEnd() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(4, 10, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(6, 12, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 4 ){
+				fail("Start port is not expected");
+			}
+			
+			if( result[0].getEndPort() != 5 ){
+				fail("End port is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutMid() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(4, 20, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(6, 12, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 2 ){
+			if( result[0].getStartPort() != 4 || result[0].getEndPort() != 5){
+				fail("First port range is not expected");
+			}
+			
+			if( result[1].getStartPort() != 13 || result[1].getEndPort() != 20){
+				fail("Second port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutStart() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(4, 20, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(1, 7, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 8 || result[0].getEndPort() != 20){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutBeforeStart() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(1, 2, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 1 || result[0].getEndPort() != 2){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutAfterEnd() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(19, 20, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(1, 7, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 19 || result[0].getEndPort() != 20){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutAtEnd() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(1, 4, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 1 || result[0].getEndPort() != 3){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutAtStart() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(7, 12, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 4 || result[0].getEndPort() != 6){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutJustAfterEnd() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(1, 4, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(5, 7, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 1 || result[0].getEndPort() != 4){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testRangeSplitCutJustBeforeStart() throws LineParseException{
+		
+		NetworkPortRange range = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange subtractRange = new NetworkPortRange(8, 12, NetworkPortRange.Protocol.TCP);
+		
+		NetworkPortRange[] result = NetworkPortRange.removeFromRange(range, subtractRange);
+		
+		if( result.length == 1 ){
+			if( result[0].getStartPort() != 4 || result[0].getEndPort() != 7){
+				fail("Port range is not expected");
+			}
+		}
+		else{
+			fail("Range is invalid");
+		}
+	}
+	
+	public void testOverlapNone(){
+		NetworkPortRange range1 = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(8, 32, NetworkPortRange.Protocol.TCP);
+		
+		if( range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do not overlap");
+		}
+	}
+	
+	public void testOverlapNone2(){
+		NetworkPortRange range1 = new NetworkPortRange(8, 32, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(4, 7, NetworkPortRange.Protocol.TCP);
+		
+		if( range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do not overlap");
+		}
+	}
+	
+	public void testOverlapAtStart(){
+		NetworkPortRange range1 = new NetworkPortRange(8, 32, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(1, 8, NetworkPortRange.Protocol.TCP);
+		
+		if( !range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do overlap");
+		}
+	}
+	
+	public void testOverlapAtEnd(){
+		NetworkPortRange range1 = new NetworkPortRange(8, 32, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(32, 48, NetworkPortRange.Protocol.TCP);
+		
+		if( !range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do overlap");
+		}
+	}
+	
+	public void testOverlapIdentical(){
+		NetworkPortRange range1 = new NetworkPortRange(8, 8, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(8, 8, NetworkPortRange.Protocol.TCP);
+		
+		if( !range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do overlap");
+		}
+	}
+	
+	public void testOverlapCompletely(){
+		NetworkPortRange range1 = new NetworkPortRange(8, 49, NetworkPortRange.Protocol.TCP);
+		NetworkPortRange range2 = new NetworkPortRange(32, 48, NetworkPortRange.Protocol.TCP);
+		
+		if( !range1.overlapsWith(range2) ){
+			fail("Incorrect response, ranges do overlap");
+		}
+	}
+	
 	public void testComputeSummary(){
 		
 		NetworkPortRange[] scanned = new NetworkPortRange[]{ new NetworkPortRange(1,5, Protocol.TCP), new NetworkPortRange(1,5, Protocol.UDP)};
