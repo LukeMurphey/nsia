31b46ad2af7714c441812e5d75fbdb0f5af921a7
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-03-13 18:17:35 +0000

    Fixed issue where ports were not highlighted in Internet Explorer

--- trunk/src/webarchive/media/misc/codepress/languages/ports.js
+++ trunk/src/webarchive/media/misc/codepress/languages/ports.js
@@ -5,7 +5,7 @@
 // ports list (UDP\162, TCP\80, etc.)
 Language.syntax = [ 
 	{ input : /\b((UDP)\\([0-9]+)([ ]*\-[ ]*([0-9]+))?)\b/gi, output : '<b>$1</b>' }, // reserved words
-	{ input : /\b((TCP)\\([0-9]+)([ ]*\-[ ]*([0-9]+))?)\b/gi, output : '<u>$1</u>' }, // special words
+	{ input : /\b((TCP)\\([0-9]+)([ ]*\-[ ]*([0-9]+))?)\b/gi, output : '<u>$1</u>' } // special words
 ]
 
 Language.snippets = []
