8bd463a5cf73463145ad78bc1225377a009a6652
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-04-09 05:48:05 +0000

    Added class that provides methods for managing strings in ThreatScripts

---
+++ trunk/src/net/lukemurphey/nsia/scan/scriptenvironment/StringUtils.java
@@ -0,0 +1,13 @@
+package net.lukemurphey.nsia.scan.scriptenvironment;
+
+public class StringUtils{
+	
+	public String trim( String str ){
+		if( str == null ){
+			return null;
+		}
+		else{
+			return str.trim();
+		}
+	}
+}
