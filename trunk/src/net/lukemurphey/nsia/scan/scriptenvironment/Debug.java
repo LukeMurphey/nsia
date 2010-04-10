517ca66acb61c23331f537ee22b1f1e13dba19aa
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-04-10 21:52:41 +0000

    Added class that allows definitions to create debugging log messages

---
+++ trunk/src/net/lukemurphey/nsia/scan/scriptenvironment/Debug.java
@@ -0,0 +1,52 @@
+package net.lukemurphey.nsia.scan.scriptenvironment;
+
+import net.lukemurphey.nsia.Application;
+import net.lukemurphey.nsia.eventlog.EventLog;
+import net.lukemurphey.nsia.eventlog.EventLogField;
+import net.lukemurphey.nsia.eventlog.EventLogMessage;
+import net.lukemurphey.nsia.eventlog.EventLogField.FieldName;
+import net.lukemurphey.nsia.eventlog.EventLogMessage.Category;
+import net.lukemurphey.nsia.scan.Definition;
+
+/**
+ * The Debug class provides definitions a mechanism to create debug log messages.
+ * This way, definition authors can add debug log messages to their definitions
+ * while writing and testing new definitions.
+ * @author Luke
+ *
+ */
+public class Debug {
+	
+	private Definition definition;
+	
+	public Debug( Definition definition ){
+		
+		// 0 -- Precondition check: make sure a definition was provided
+		if( definition == null ){
+			throw new IllegalArgumentException("The definition argument cannot be null");
+		}
+		
+		// 1 -- Configure the class
+		this.definition = definition;
+	}
+	
+	public void sendMessage( String message ){
+		
+		// 0 -- Make sure a valid message was provided.
+		if( message == null ){
+			return;//No message to send
+		}
+		
+		// 1 -- Create the log message
+		EventLog log = Application.getApplication().getEventLog();
+		
+		EventLogMessage eventLogMessage = new EventLogMessage(Category.DEFINITION_DEBUG_MESSAGE,
+				new EventLogField(FieldName.MESSAGE, message),
+				new EventLogField(FieldName.DEFINITION_ID, definition.getID()),
+				new EventLogField(FieldName.DEFINITION_REVISION, definition.getRevision()),
+				new EventLogField(FieldName.DEFINITION_NAME, definition.getFullName()));
+		
+		log.logEvent(eventLogMessage);
+	}
+
+}
