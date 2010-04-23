549d02c088f90ee98f064dde5c4295cee6266e5c
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-04-23 06:16:48 +0000

    Updated application to version 0.8.98

--- trunk/src/net/lukemurphey/nsia/Application.java
+++ trunk/src/net/lukemurphey/nsia/Application.java
@@ -45,7 +45,7 @@ public final class Application {
 	
 	public static final int VERSION_MAJOR = 0;
 	public static final int VERSION_MINOR = 8;
-	public static final int VERSION_REVISION = 97;
+	public static final int VERSION_REVISION = 98;
 	public static final String VERSION_STATUS = null;
 	public static final String DATABASE_LOCATION = "../var/database";
 	
