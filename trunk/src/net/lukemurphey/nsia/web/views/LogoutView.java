b1d09cdc041a17f7a63e8d20de10f150330d667b
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-10-09 05:04:37 +0000

    Added log message noting that user ended their session (logged out)

--- trunk/src/net/lukemurphey/nsia/web/views/LogoutView.java
+++ trunk/src/net/lukemurphey/nsia/web/views/LogoutView.java
@@ -12,6 +12,7 @@ import net.lukemurphey.nsia.Application;
 import net.lukemurphey.nsia.InputValidationException;
 import net.lukemurphey.nsia.NoDatabaseConnectionException;
 import net.lukemurphey.nsia.SessionManagement;
+import net.lukemurphey.nsia.SessionManagement.SessionInfo;
 import net.lukemurphey.nsia.eventlog.EventLogField;
 import net.lukemurphey.nsia.eventlog.EventLogMessage;
 import net.lukemurphey.nsia.eventlog.EventLogField.FieldName;
@@ -51,8 +52,24 @@ public class LogoutView extends View {
 			Application app = Application.getApplication();
 			
 			SessionManagement session_mgmt = new SessionManagement(app);
+			
 			try {
+				
+				// Create the log message noting that session
+				SessionInfo sessionInfo = session_mgmt.getSessionInfo(sessionID);
+				
+				EventLogMessage message = new EventLogMessage( EventLogMessage.EventType.SESSION_ENDED,
+						new EventLogField( FieldName.TARGET_USER_NAME, sessionInfo.getUserName() ),
+						new EventLogField( FieldName.TARGET_USER_ID, sessionInfo.getUserId() ),
+						new EventLogField( FieldName.SESSION_TRACKING_NUMBER, sessionInfo.getTrackingNumber() )
+				);
+				
+				// Terminate the session
 				session_mgmt.terminateSession(sessionID);
+				
+				// Send the log message noting that the session was ended
+				app.logEvent(message);
+				
 			} catch (InputValidationException e) {
 				app.logEvent(EventLogMessage.EventType.SESSION_ID_ILLEGAL, new EventLogField( FieldName.SESSION_ID, sessionID ) );
 			} catch (SQLException e) {
