43a1e2d36664a6c45ef54eadb3a0a51acc33a735
Luke <Luke@LukeMurphey.net>
2010-10-29 07:41:55 +0000

    Added an argument for the dashboard refresh panel template to note that the view was recreated via an ajax call

--- src/net/lukemurphey/nsia/web/views/DashboardRefreshPanel.java
+++ src/net/lukemurphey/nsia/web/views/DashboardRefreshPanel.java
@@ -13,12 +13,16 @@ public class DashboardRefreshPanel {
 	
 	public String getPanel( HttpServletRequest request, Map<String, Object> data, String url, HttpServletResponse response ) throws ViewFailedException{
 			
+		// Get the refresh rate from the cookie if it exists
 		String refreshRate = request.getParameter("refreshRate");
 		Cookie[] cookies = request.getCookies();
 		
+		// Add the cookie if it does not already exist and was provided via the argument
 		if( refreshRate != null ){
 			response.addCookie(new Cookie("RefreshRate", refreshRate));
 		}
+		
+		// Otherwise, try to find the refresh rate
 		else{
 			for( int c = 0; c < cookies.length; c++){
 				if( cookies[c].getName().matches("RefreshRate") )
@@ -26,14 +30,27 @@ public class DashboardRefreshPanel {
 			}
 		}
 		
+		// Put the refresh URL in the template 
 		data.put("refresh_url", url);
 		
+		// Assign a default refresh rate
 		if( refreshRate == null ){
-			refreshRate = "60";
+			refreshRate = "30";
 		}
 		
-		data.put("refresh_rate", refreshRate);		
+		// Put the refresh rate in the template 
+		data.put("refresh_rate", refreshRate);
 		
+		// Put an argument in the list of the view is being refreshed via an AJAX call
+		boolean isajax = false;
+		
+		if( request.getParameter("isajax") != null ){
+			isajax = true;
+		}
+		
+		data.put("isajax", isajax);
+		
+		// Render the template
 		return TemplateLoader.renderToString("DashboardRefreshEntry.ftl", data);
 	}
 
