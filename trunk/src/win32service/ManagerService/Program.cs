3fadea8a1af9214ae74ea3fd3185a11c973a9139
lukemurphey <lukemurphey@e2138058-fc44-4d37-9784-1678a3267be6>
2010-11-28 06:28:19 +0000

    Removed unused code for installing and uninstalling the service

--- trunk/src/win32service/ManagerService/Program.cs
+++ trunk/src/win32service/ManagerService/Program.cs
@@ -17,31 +17,6 @@ namespace ThreatFactor.NSIA.Service
         [STAThread]
         static void Main(string[] Args)
         {
-            // 1 -- Determine if the user is attempting to install or uninstall the service
-            Boolean install = false;
-            Boolean uninstall = false;
-
-            if (Args.Length > 1 && Args[1].Equals("install", StringComparison.CurrentCultureIgnoreCase))
-                install = true;
-
-            if (Args.Length > 1 && Args[1].Equals("uninstall", StringComparison.CurrentCultureIgnoreCase))
-                uninstall = true;
-            
-            // 1.1 -- Install the service and exit
-            if (install)
-            {
-                ManagerServiceInstaller serviceInstaller = new ManagerServiceInstaller();
-                serviceInstaller.Install();
-            }
-
-            // 1.2 -- Uninstall the service and exit
-            if (uninstall)
-            {
-                ManagerServiceInstaller serviceInstaller = new ManagerServiceInstaller();
-                serviceInstaller.Uninstall();
-            }
-
-            // 2 -- Run the service
             ServiceBase.Run(new NSIAService());
         }
     }
