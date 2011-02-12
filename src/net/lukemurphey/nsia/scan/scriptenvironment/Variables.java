d208321f3badd3d52d27313424a8f7de08a8f596
Luke <Luke@LukeMurphey.net>
2011-02-12 07:50:20 +0000

    Added ability for variables to contain values. This way, scripts can store values that are not persisted to the database and are alive only for the execution time of the rule on a given URL.

--- src/net/lukemurphey/nsia/scan/scriptenvironment/Variables.java
+++ src/net/lukemurphey/nsia/scan/scriptenvironment/Variables.java
@@ -4,11 +4,43 @@ import java.util.Vector;
 
 public class Variables {
 	
-	private Vector<String> vars = new Vector<String>();
+	private class Variable{
+		String name;
+		Object value = null;
+		
+		public Variable(String name){
+			
+			// Precondition check
+			if( name == null ){
+				throw new IllegalArgumentException();
+			}
+			this.name = name;
+		}
+		
+		public Variable(String name, Object value){
+			this(name);
+			
+			this.value = value;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		
+		public Object getValue(){
+			return value;
+		}
+		
+		public void setValue(Object value){
+			this.value = value;
+		}
+	}
+	
+	private Vector<Variable> vars = new Vector<Variable>();
 	
 	public boolean isSet( String variable ){
 		for( int c = 0; c < vars.size(); c++){
-			if( vars.get(c).equalsIgnoreCase(variable) ){
+			if( vars.get(c).getName().equalsIgnoreCase(variable) ){
 				return true;
 			}
 		}
@@ -18,7 +50,7 @@ public class Variables {
 	
 	public void unSet( String variable ){
 		for( int c = 0; c < vars.size(); c++){
-			if( vars.get(c).equalsIgnoreCase(variable) ){
+			if( vars.get(c).getName().equalsIgnoreCase(variable) ){
 				vars.remove(c);
 				return;
 			}
@@ -26,9 +58,39 @@ public class Variables {
 	}
 	
 	public void set(String variable){
-		if( !isSet(variable) ){
-			vars.add(variable);
+		set( variable, null );
+	}
+	
+	public void set(String name, Object value){
+		
+		Variable variable = getVariable(name);
+		
+		if( variable == null ){
+			vars.add( new Variable(name, value) );
 		}
+		else{
+			variable.setValue(value);
+		}
+	}
+	
+	private Variable getVariable(String name){
+		for( int c = 0; c < vars.size(); c++){
+			if( vars.get(c).getName().equalsIgnoreCase(name) ){
+				return vars.get(c);
+			}
+		}
+		
+		return null;
+	}
+	
+	public Object get(String name){
+		Variable variable = getVariable(name);
+		
+		if( variable != null ){
+			return variable.getValue();
+		}
+		
+		return null;
 	}
 	
 	public int size(){
@@ -36,7 +98,13 @@ public class Variables {
 	}
 	
 	public String get(int c){
-		return vars.get(c);
+		Variable var = vars.get(c);
+		
+		if( var != null ){
+			return var.getName();
+		}
+		
+		return null;
 	}
 
 }
